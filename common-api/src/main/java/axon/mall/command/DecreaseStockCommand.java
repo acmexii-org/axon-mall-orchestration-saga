42c38d5385bc2018ac9b29b2b6ed3d7b82f510dd
박용주 <apexacme@gmail.com>
2023-03-21 17:13:10 +0900

    commit

---
+++ common-api/src/main/java/axon/mall/command/DecreaseStockCommand.java
@@ -0,0 +1,14 @@
+package axon.mall.command;
+
+import java.util.List;
+import lombok.Data;
+import lombok.ToString;
+import org.axonframework.modelling.command.TargetAggregateIdentifier;
+
+@ToString
+@Data
+public class DecreaseStockCommand {
+
+    @TargetAggregateIdentifier
+    private String productId;
+}
---
+++ product/src/main/java/axon/mall/aggregate/ProductAggregate.java
@@ -0,0 +1,49 @@
+package axon.mall.aggregate;
+
+import static org.axonframework.modelling.command.AggregateLifecycle.*;
+
+import axon.mall.command.*;
+import axon.mall.event.*;
+import axon.mall.query.*;
+import java.util.List;
+import java.util.UUID;
+import lombok.Data;
+import lombok.ToString;
+import org.axonframework.commandhandling.CommandHandler;
+import org.axonframework.eventsourcing.EventSourcingHandler;
+import org.axonframework.modelling.command.AggregateIdentifier;
+import org.axonframework.spring.stereotype.Aggregate;
+import org.springframework.beans.BeanUtils;
+
+@Aggregate
+@Data
+@ToString
+public class ProductAggregate {
+
+    @AggregateIdentifier
+    private String productId;
+
+    private String productName;
+    private String productNo;
+    private Integer stock;
+
+    public ProductAggregate() {}
+
+    @CommandHandler
+    public void handle(DecreaseStockCommand command) {
+        StockDecreasedEvent event = new StockDecreasedEvent();
+        BeanUtils.copyProperties(command, event);
+
+        apply(event);
+    }
+
+    private String createUUID() {
+        return UUID.randomUUID().toString();
+    }
+
+    @EventSourcingHandler
+    public void on(StockDecreasedEvent event) {
+        //TODO: business logic here
+
+    }
+}
